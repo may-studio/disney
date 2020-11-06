b8d19bd3ae20fca9e7d78b611e9c850087a976ec
may-studio <>
2020-11-06 22:29:14 +0300

    Updates

---
+++ precache-manifest.baef0caf70c66b973a35fb4b08931e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a017136b4df72cc468ee919c30b9568e",
+    "url": "/disney/index.html"
+  },
+  {
+    "revision": "a050883036aeae20532b",
+    "url": "/disney/static/css/main.d86c473c.chunk.css"
+  },
+  {
+    "revision": "400304ea5c2d677a0528",
+    "url": "/disney/static/js/2.db8e3a73.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/disney/static/js/2.db8e3a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a050883036aeae20532b",
+    "url": "/disney/static/js/main.f7fd7020.chunk.js"
+  },
+  {
+    "revision": "c7fe8dd581b621f938af",
+    "url": "/disney/static/js/runtime-main.e318d25d.js"
+  }
+]);
